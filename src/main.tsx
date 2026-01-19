4cac42a2528c8a79a4fa34034cf73971d34f1427
Alex Zhao <zhaochunqi@users.noreply.github.com>
2026-01-19 19:36:21 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,62 @@
+import "@logseq/libs";
+
+import React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+import { logseq as PL } from "../package.json";
+
+// @ts-expect-error
+const css = (t, ...args) => String.raw(t, ...args);
+
+const pluginId = PL.id;
+
+function main() {
+  console.info(`#${pluginId}: MAIN`);
+  const root = ReactDOM.createRoot(document.getElementById("app")!);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+
+  function createModel() {
+    return {
+      show() {
+        logseq.showMainUI();
+      },
+    };
+  }
+
+  logseq.provideModel(createModel());
+  logseq.setMainUIInlineStyle({
+    zIndex: 11,
+  });
+
+  const openIconName = "template-plugin-open";
+
+  logseq.provideStyle(css`
+    .${openIconName} {
+      opacity: 0.55;
+      font-size: 20px;
+      margin-top: 4px;
+    }
+
+    .${openIconName}:hover {
+      opacity: 0.9;
+    }
+  `);
+
+  logseq.App.registerUIItem("toolbar", {
+    key: openIconName,
+    template: `
+    <a data-on-click="show">
+        <div class="${openIconName}">⚙️</div>
+    </a>    
+`,
+  });
+}
+
+logseq.ready(main).catch(console.error);
